176e68da650bca5a38e39daa640df1643a1762d8
vladimir <vlado.sedlar@gmail.com>
2025-07-01 20:59:01 +0200

    update

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from myproject.boards.models import Board
+
 
 def home(request):
-    return HttpResponse("Hello, World!!!")
+    boards = Board.objects.all()
+    boards_names = []
+
+    for board in boards:
+        boards_names.append(board.name)
+
+    html = "<br>".join(boards_names)
+
+    return HttpResponse(html)
