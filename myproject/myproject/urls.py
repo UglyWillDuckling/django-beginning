e03f8b64c0c6be1c7fbb85705ecd4c0eede964e1
vladimir <vlado.sedlar@gmail.com>
2025-07-18 00:35:10 +0200

    add login,logout routes + form tags

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,13 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, re_path
-
+from django.contrib.auth import views as auth_views
 from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     re_path(r"^$", views.home, name="home"),
     re_path(r"^signup/$", accounts_views.signup, name="signup"),
+    re_path(
+        r"^login/$",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    re_path(r"^logout/$", auth_views.LogoutView.as_view(), name="logout"),
     re_path("^boards/(?P<pk>\d+)/$", views.board_topics, name="board_topics"),
     re_path("^boards/(?P<pk>\d+)/new/$", views.new_topic, name="new_topic"),
     path("admin/", admin.site.urls),
